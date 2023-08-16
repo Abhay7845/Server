11b7bc171716bb0c0462ef60334a3a5a69028078
abhay kumar <abhaykr95678@gmail.com>
2023-08-16 22:31:55 +0530

    updated

--- Index.js
+++ Index.js
@@ -12,6 +12,8 @@ const PORT = process.env.PORT || 5000;
 
 // Available Routes
 app.use("/api/user", require("./src/routes/User"));
+app.use("/api/user", require("./src/routes/GenerateOtp"));
+
 app.listen(PORT, () => {
   console.log(`Server is Running on PORT ${PORT}`);
 });
