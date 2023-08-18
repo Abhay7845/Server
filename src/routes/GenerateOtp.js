b40daa95c1d25c61da40bca00e5eccb9738060c7
abhay kumar <abhaykr95678@gmail.com>
2023-08-18 14:36:03 +0530

    updated'

--- src/routes/GenerateOtp.js
+++ src/routes/GenerateOtp.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const twilio = require("twilio");
+const User = require("../model/Users");
+const nodemailer = require("nodemailer");
 
 const accountSid = "AC87c4448d2dacc9bce7b75f78e9606b66";
 const authToken = "5fc3fb4937bb24a05ea691183c41797f";
 const twilioPhone = +19519042773;
 const client = new twilio(accountSid, authToken);
 
-router.post("/send-otp", async (req, res) => {
+const emailConfig = {
+  service: "your_email_service_provider", // e.g., 'Gmail'
+  auth: {
+    user: "your_email@example.com",
+    pass: "your_email_password",
+  },
+};
+
+const transporter = nodemailer.createTransport(emailConfig);
+
+router.post("/send-otp/by/phone", async (req, res) => {
   const { phoneNumber } = await req.body;
   const otp = Math.floor(100000 + Math.random() * 900000);
   if (!phoneNumber) {
@@ -32,6 +44,42 @@ router.post("/send-otp", async (req, res) => {
       });
   } catch (error) {
     console.log("error=>", error);
+  }
+});
+
+router.post("/send-otp/by/email", async (req, res) => {
+  const { email } = await req.body;
+  const otp = Math.floor(100000 + Math.random() * 900000);
+  if (!email) {
+    return res.status(400).json({ message: "email is required" });
+  }
+  try {
+    let user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).send({ success: false, message: "invalid email" });
+    } else {
+      const mailOptions = {
+        from: "your_email@example.com",
+        to: email,
+        subject: "Your OTP Code",
+        text: `Your OTP code is: ${otp}`,
+      };
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.error("Error sending email==>", error);
+          return res.status(500).json({ message: "Failed to send OTP email" });
+        } else {
+          console.log("Email sent==>", info.response);
+          res.json({
+            success: true,
+            message: "OTP has been sent successfully",
+            otp: otp,
+          });
+        }
+      });
+    }
+  } catch (error) {
+    console.log("error==>", error);
   }
 });
 
