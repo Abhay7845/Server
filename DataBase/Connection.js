c6cf70728e6d78e69d7cd7cec265df779beccdd4
Abhaykumar(Iteanz) <iteanzabhaykumar@titan.co.in>
2024-07-28 10:46:29 +0530

    updated db connection

--- DataBase/Connection.js
+++ DataBase/Connection.js
@@ -1,21 +1,13 @@
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const ENV = require("../.env");
 mongoose.set("strictQuery", false);
 
-const DataBaseConnectionURI = "mongodb://127.0.0.1:27017/StudentsData";
-
-dotenv.config(ENV);
-// const DataBaseConnectionURI = process.env.DATABASE_URL;
+const DataBaseConnectionURI = "mongodb+srv://softwaredeveloper638:softwareDeveloper638@collagedata.vagk1j8.mongodb.net/";
 
 const DatabaseConnection = () => {
-  mongoose
-    .connect(DataBaseConnectionURI, {
-      useNewUrlParser: true,
-      useUniFiedTopology: true,
-    })
-    .then(() => console.log("Connection Successfully"))
-    .catch((error) => console.log("error==>", error));
+  mongoose.connect(DataBaseConnectionURI, {
+    useNewUrlParser: true,
+    useUniFiedTopology: true,
+  }).then(() => console.log("Connection Successfully")).catch((error) => console.log("error==>", error));
 };
 
 module.exports = DatabaseConnection;
--- Index.js
+++ Index.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv");
-const ENV = require("./.env");
 const app = express();
 const connectTOdb = require("./DataBase/Connection");
 connectTOdb();
 app.use(express.json());
 app.use(cors());
-dotenv.config(ENV);
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 // Available Routes
 app.use("/api/user", require("./src/routes/User"));
@@ -16,6 +13,4 @@ app.use("/api/user", require("./src/routes/Login"));
 app.use("/api/user", require("./src/routes/GenerateOtp"));
 app.use("/api/user", require("./src/routes/ContactUs"));
 
-app.listen(PORT, () => {
-  console.log(`Server is Running on PORT ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server is Running on PORT ${PORT}`));
