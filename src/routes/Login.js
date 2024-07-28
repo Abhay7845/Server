ed1146a312572bb54e53eb5e8df3951635daf1b6
Abhaykumar(Iteanz) <iteanzabhaykumar@titan.co.in>
2024-07-28 18:33:50 +0530

    updated

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -20,9 +20,9 @@ router.post("/login", loginValidation, async (req, res) => {
     if (!user) {
       return res.status(200).send({ success: false, error: "Sorry!  please register with us" });
     }
-    const comparePassword = bcrypt.compare(password, user.password);
+    const comparePassword = await bcrypt.compare(password, user.password);
     if (!comparePassword) {
-      return res.status(200).send({ success: false, error: "Sorry!  password dose not matched" });
+      return res.status(200).send({ success: false, error: "Sorry! password dose not matched" });
     }
     const data = {
       user: user,
@@ -62,5 +62,6 @@ router.get("/login/success/:email", async (req, res) => {
     return res.status(500).send({ success: false, massage: "Internal Servr Error" });
   }
 });
+
 
 module.exports = router;
