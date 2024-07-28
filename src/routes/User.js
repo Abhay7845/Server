96ae85388ca803cedf61822b2afe74f4db60ae62
Abhaykumar(Iteanz) <iteanzabhaykumar@titan.co.in>
2024-07-28 18:58:35 +0530

    updated

--- src/routes/User.js
+++ src/routes/User.js
@@ -4,7 +4,6 @@ const bcrypt = require("bcryptjs");
 const User = require("../model/Users");
 const AddUser = require("../model/AddUser");
 const addUserValidation = require("../validation/addUser");
-const registerValidation = require("../validation/Register");
 const { validationResult } = require("express-validator");
 var jwt = require("jsonwebtoken");
 var fetchUser = require("../middleware/FetchUser");
@@ -13,23 +12,24 @@ const JWT_SECRET = "AryanIsGoodBoy";
 
 const loginTime = Date();
 // REGISTER ROUTER :-1
-router.post("/register", registerValidation, async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).send({ success: false, errors: errors.array() });
-  }
+router.post("/register", async (req, res) => {
+  const { name, email, phone, password } = await req.body;
+  if (!name) return res.status(200).send({ status: false, message: "name is required" });
+  if (!email) return res.status(200).send({ status: false, message: "email is required" });
+  if (!phone) return res.status(200).send({ status: false, message: "phone is required" });
+  if (!password) return res.status(200).send({ status: false, message: "password is required" });
   try {
-    let user = await User.findOne({ email: req.body.email });
+    let user = await User.findOne({ email: email });
     if (user) {
       return res.status(200).json({ success: false, massage: "sorry! Email is already registered" });
     }
     const salt = await bcrypt.genSalt(10);
-    const SecPassword = bcrypt.hashSync(req.body.password, salt);
+    const SecPassword = bcrypt.hashSync(password, salt);
     // create users
     user = await User.create({
-      name: req.body.name,
-      email: req.body.email,
-      phone: req.body.phone,
+      name: name,
+      email: email,
+      phone: phone,
       password: SecPassword,
     });
     const data = {
