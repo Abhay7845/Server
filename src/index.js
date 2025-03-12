b9259298fc3eb54ead60468d28df79e6a09929a4
Abhaykumar(Iteanz) <iteanzabhaykumar@titan.co.in>
2025-03-12 11:52:48 +0530

    change db name

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ const express = require("express");
 const cors = require("cors");
 require('dotenv').config();
 const app = express();
-const connectTOdb = require("./src/dataBase/Connection");
+const connectTOdb = require("./dataBase/Connection");
 connectTOdb(process.env.DB_URL);
 app.use(express.json());
 app.use(cors());
 const PORT = process.env.PORT || 7000;
 
 // Available Routes
-app.use("/api/user", require("./src/routes/User"));
-app.use("/api/user", require("./src/routes/Login"));
-app.use("/api/user", require("./src/routes/GenerateOtp"));
-app.use("/api/user", require("./src/routes/ContactUs"));
+app.use("/api/user", require("./routes/User"));
+app.use("/api/user", require("./routes/Login"));
+app.use("/api/user", require("./routes/GenerateOtp"));
+app.use("/api/user", require("./routes/ContactUs"));
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
